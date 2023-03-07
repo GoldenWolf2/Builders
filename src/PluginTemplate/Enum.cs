79c9a8a678cf25f6be86d1ebfee800cf6722f681
That Blackwolf <107058543+imnagarajan@users.noreply.github.com>
2023-03-07 19:17:40 +0530

    Create Enum.cs

---
+++ src/PluginTemplate/Enum.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGamesAPI
+{
+    public static class Enum
+    {
+        public enum RoomStatus{
+        Waiting,
+        Selecting,
+        Gaming,
+        Concluding,
+        Restoring,
+        Stopped
+        }
+        public enum PlayerStatus { 
+        Waiting,
+        Selecting,
+        Gaming    
+        }
+        public enum TeamID
+        {
+            White=0,
+            Red,
+            Green,
+            Blue,
+            Yellow,
+            Pink
+        }
+    }
+}
